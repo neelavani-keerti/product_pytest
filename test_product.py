575f2dac7f186f5d3ddecad28fb2f7992bf852e0
neelavani-keerti <keertineelavani@gmail.com>
2025-12-11 16:42:55 +0530

    Create test_product.py

---
+++ test_product.py
@@ -0,0 +1,10 @@
+
+from product import product_details
+def test_product_details():
+    expected_output=(
+        "Product Name:Keerti\n"
+        "Product ID:E193\n"
+        "Quantity:40g\n"
+        "Price:20\n"
+    )
+    assert product_details("KitKat","K193","40g",20)== expected_output
