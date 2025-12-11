395a97495fb390a34925ebacae1154900f428931
neelavani-keerti <keertineelavani@gmail.com>
2025-12-11 16:36:28 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,14 @@
+def product_details(name,pro_id,qty,price):
+    result=(
+        f"product Name:{name}\n"
+        f"Product ID:{pro_id}\n"
+        f"Quantity:{qty}\n"
+        f"Price:{price}\n"
+    )
+    return result
+if __name__ == "__main__":
+    name="KitKat"
+    emp_id="K193"
+    qty="40g"
+    price=20
+    print(product_details(name,pro_id,qty,price))
