dcc37475c5e7214dcbf7070f794a6713e0741270
neelavani-keerti <keertineelavani@gmail.com>
2025-12-14 20:38:47 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,10 +1,11 @@
-
 from product import product_details
+
 def test_product_details():
-    expected_output=(
+    expected_output = (
         "Product Name:KitKat\n"
         "Product ID:K193\n"
         "Quantity:40g\n"
         "Price:20\n"
     )
-    assert product_details("KitKat","K193","40g",20)== expected_output
+    assert product_details("KitKat", "K193", "40g", 20) == expected_output
+
