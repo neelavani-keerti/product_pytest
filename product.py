9eb10b680ac08a660009e377fe6901d6588fb2e3
neelavani-keerti <keertineelavani@gmail.com>
2025-12-14 20:36:50 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,14 +1,17 @@
-def product_details(name,pro_id,qty,price):
-    result=(
-        f"product Name:{name}\n"
-        f"Product ID:{pro_id}\n"
-        f"Quantity:{qty}\n"
-        f"Price:{price}\n"
+def product_details(name, pro_id, qty, price):
+    result = (
+        f"Product Name: {name}\n"
+        f"Product ID: {pro_id}\n"
+        f"Quantity: {qty}\n"
+        f"Price: {price}\n"
     )
     return result
+
 if __name__ == "__main__":
-    name="KitKat"
-    emp_id="K193"
-    qty="40g"
-    price=20
-    print(product_details(name,pro_id,qty,price))
+    name = "KitKat"
+    pro_id = "K193"
+    qty = "40g"
+    price = 20
+
+    print(product_details(name, pro_id, qty, price))
+
